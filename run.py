b70173b1c54a1e0a3a8b4c41d7b68080827cca35
Risa Ohara <risa.ohara@gmail.com>
2014-12-07 00:54:54 -0500

    first commit

---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from app import app
+app.run(debug=True)
+
+#import os
+#import sys
+
+#sys.path.insert(1, os.path.join(os.path.abspath('.'), 'lib'))
+#from app import app
---
+++ tests.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+import unittest
+
+from config import basedir
+from app import app, db
+from app.models import User
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
+            basedir, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_avatar(self):
+        u = User(nickname='john', email='john@example.com')
+        avatar = u.avatar(128)
+        expected = 'http://wwww.gravatar.com/avatar/d4c74594d841139328695756648b6bd6'
+        assert avatar[0:len(expected)] == expected
+
+    def test_make_unique_nick(self):
+        u = User(nickname='john', email='john.example.com')
+        db.session.add(u)
+        db.session.commit()
+        nickname2 = User.make_unique_nickname('john')
+        assert nickname2 != 'john'
+        assert nickname2 != nickname
+
+if __name__ == '__main__':
+    unittest.main()
